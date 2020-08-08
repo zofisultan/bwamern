3845100901bb2a27c395b5d505d12e312861745b
zofisultan <zofinofriandi23@gmail.com>
2020-08-08 08:24:20 +0700

    Update Header.js

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import Button from 'elements/Button'
 
-export default function Header() {
+export default function Header(props) {
     return (
         <div>
             
