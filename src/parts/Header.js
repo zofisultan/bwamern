1fef598d0e5470786b97f45bc177b2a0b719cda1
zofisultan <zofinofriandi23@gmail.com>
2020-08-08 08:30:22 +0700

    Update Header.js

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Button from 'elements/Button'
+import BrandIcon from 'parts/IconText'
 
 export default function Header(props) {
+    
     return (
-        <div>
-            
-        </div>
+        <header className="spacing-sm">
+            <div className="container">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <BrandIcon />
+                </nav>
+            </div>            
+        </header>
     )
 }
 
