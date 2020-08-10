d314dd7a24948d55d66fa25e2d67dd26a8555bde
zofisultan <zofinofriandi23@gmail.com>
2020-08-10 16:36:17 +0700

    commit on master

--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 
 import Header from 'parts/Header';
 
+import landingPage from 'json/landingPage.json';
+import Hero from 'parts/Hero';
+
+
 export default class Landingpage extends Component {
     render() {
+        console.log(this.props);
         return <>
-           <Header {...this.props}></Header>     
+           <Header {...this.props}></Header>  
+           <Hero data={landingPage.hero}/>
         </>
     }
 }
